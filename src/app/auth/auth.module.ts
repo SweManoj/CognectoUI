01e2aaf27efbacb0e556a5ecac487c8dedf462ea
sachinshetty824 <51442183+sachinshetty824@users.noreply.github.com>
2020-02-10 14:50:34 +0530

    first basic commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { NbLayoutModule } from '@nebular/theme';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [AuthComponent, LoginComponent, RegisterComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    NbLayoutModule,
+  ]
+})
+export class AuthModule { }
